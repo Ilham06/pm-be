bd4a1a343983e3769d51c70df78beeb20da23408
ilhamvorta <ilham@vorta.live>
2025-04-30 12:08:50 +0700

    update env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World, Ini test perubahan ketiga!';
+    return 'Hello World, Ini test perubahan keempat build version!';
   }
 }
