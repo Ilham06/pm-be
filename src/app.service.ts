e0e866413a9b69e2354b432d8f1df54308e37bea
ilhamvorta <ilham@vorta.live>
2025-05-14 13:06:34 +0700

    updte jenkinsfile

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World, Ini test perubahan keenam. branch dev & prod!';
+    return 'Hello World, Ini test perubahan kesepuluh. branch dev & prod!';
   }
 }
